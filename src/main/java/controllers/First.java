125f19edf0dfc715f263ecbf2583ec3474619de7
934205 <nadesanjayarani4@gmail.com>
2026-02-16 16:27:54 +0530

    changed

--- src/main/java/controllers/First.java
+++ src/main/java/controllers/First.java
@@ -1,4 +1,13 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class First {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "welcom";
+    }
 }
