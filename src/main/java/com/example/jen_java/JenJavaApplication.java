bb787f18cf893c9817f33af69324314be9644363
934205 <nadesanjayarani4@gmail.com>
2026-02-16 16:19:43 +0530

    first

---
+++ src/main/java/com/example/jen_java/JenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jen_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jen_java/JenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jen_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
